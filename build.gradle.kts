e3d3a44f1292375d516d43c12f2f729c4ecfb22c
yuu0922 <goldeneye1119yugo@gmail.com>
2025-09-16 12:48:28 +0900

    build: Update AGP and Gradle properties
    - Update Android Gradle Plugin (AGP) to version 9.0.0-alpha05 in `build.gradle.kts`.
    - Set `android.builtInKotlin=false` in `gradle.properties`.
    - Set `android.newDsl=false` in `gradle.properties`.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "9.0.0-alpha02" apply false
+    id("com.android.application") version "9.0.0-alpha05" apply false
     id("org.jetbrains.kotlin.android") version "2.2.20" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.20" apply false
     id("com.google.devtools.ksp") version "2.2.20-2.0.3" apply false
